ba6ce8ead62dfae5b8b227bf15df0dd4279bbc37
Rohit Ambre <rohit.ambre@unotechsoft.com>
2019-07-14 15:39:22 +0530

    added env file and url validator

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
 var cors = require('cors');
+const { URL } = require('url');
+const dns = require('dns');
 
 var app = express();
 
@@ -12,12 +14,14 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+console.log(process.env.MONGO_URI)
+// mongoose.connect(process.env.MONGO_URI, { useMongoClient: true });
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -30,6 +34,25 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+
+app.post("/api/shorturl/new",function(req,res){
+  const longUrl = new URL(req.body.url);
+
+  dns.lookup(longUrl.hostname,function(err,address, family){
+    if (err){
+      console.log(err)
+      res.json({"error":"invalid URL"});
+    }else{
+      res.json({"fj":req.body.url})
+    }
+  })
+  
+})
+
+app.get("api/shorturl/:new_url",function(req,res){
+  
+})
 
 
 app.listen(port, function () {
